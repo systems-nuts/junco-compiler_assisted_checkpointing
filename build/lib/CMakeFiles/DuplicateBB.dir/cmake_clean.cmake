d4203d7f3dc45c3b3f693ea3aa3cf791171a2170
dalehuangzihan <huangzihan98@gmail.com>
2022-10-22 14:47:34 +0100

    added popcorn compiler and llvm-14 build

---
+++ build/lib/CMakeFiles/DuplicateBB.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DuplicateBB.dir/DuplicateBB.cpp.o"
+  "CMakeFiles/DuplicateBB.dir/DuplicateBB.cpp.o.d"
+  "libDuplicateBB.pdb"
+  "libDuplicateBB.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DuplicateBB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
