d30ab0f1e984a56610cead271ec11d0d23e8bdda
dalehuangzihan <huangzihan98@gmail.com>
2023-02-22 19:09:50 +0000

    revert lud test to size 16 (passes)

--- examples/lud_xrt/src/lud.h
+++ examples/lud_xrt/src/lud.h
@@ -26,7 +26,7 @@
 
 // #define CKPT_SIZE 4
 // CKPT_SIZE = (num of discrete metadata) + (largest val from ckpt_sizes_bytes_json) / 4
-#define CKPT_SIZE (4*4 + 6 + 3) // must have brackets around arithmetic!
+#define CKPT_SIZE (16*16 + 6 + 3) // must have brackets around arithmetic!
 
 #define TYPE float
 
